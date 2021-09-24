a554d0554c26caddbac0852d8249ca0a44d1e85a
Sabelius <sbebby92@virgilio.it>
2021-09-24 14:28:03 +0200

    inizializzazione esercizio con inizio header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+      <img src="../assets/img/dc-logo.png" alt="header-logo" />
+
+
+      <nav>
+          <ul>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+          </ul>
+      </nav>
+    </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
